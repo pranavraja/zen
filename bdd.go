90fc8461b92b4e8ef48542f279bbc427471d8273
Pranav Raja <rickdangerous1@gmail.com>
2013-07-11 21:48:19 +1000

    Consolidate test.go into bdd.go

--- bdd.go
+++ bdd.go
@@ -2,14 +2,26 @@ package mao
 
 import (
 	"fmt"
+	"testing"
 )
+
+type Test struct {
+	T     *testing.T
+	Title string
+	Fn    func(Expect)
+}
+
+func (test *Test) Run() {
+	test.Fn(func(val interface{}) *Expectation {
+		return &Expectation{test, val}
+	})
+}
 
 type It func(title string, fn func(Expect))
 
-func Desc (desc string, wrapper func(It) ) {
-
-	wrapper(func (it string, fn func(Expect)) {
-		test := Test{ fmt.Sprintf("%s %s", desc, it), fn }
+func Desc(t *testing.T, desc string, wrapper func(It)) {
+	wrapper(func(it string, fn func(Expect)) {
+		test := Test{t, fmt.Sprintf("%s %s", desc, it), fn}
 		test.Run()
 	})
 
