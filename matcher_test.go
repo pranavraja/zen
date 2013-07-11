265977d49b57b23eceb88e41d1769ef5269e2168
Pranav Raja <rickdangerous1@gmail.com>
2013-07-11 21:50:45 +1000

    Refactor matchers, allow custom matchers

---
+++ matcher_test.go
@@ -0,0 +1,60 @@
+package mao
+
+import (
+	"testing"
+)
+
+func differsByOne(a, b interface{}) bool {
+	diff := a.(int) - b.(int)
+	return diff == 1 || diff == -1
+}
+
+type output struct {
+	titlePrinted bool
+	errors       string
+}
+
+func (out *output) PrintTitle() {
+	out.titlePrinted = true
+}
+func (out *output) PrintError(err string) {
+	out.errors += err
+}
+
+// Prints title, even when test passes
+func TestMatcherPrintsTitle(t *testing.T) {
+	out := new(output)
+	expectation := &Expectation{out, 1}
+	expectation.ToEqual(1)
+	if titlePrinted := out.titlePrinted; !titlePrinted {
+		t.Errorf("should have printed title, but didn't")
+	}
+}
+
+// Prints custom error message
+func TestCustomMatcher(t *testing.T) {
+	out := new(output)
+	expectation := &Expectation{out, 1}
+	expectation.To("differ by one from", differsByOne, 10)
+	if err := out.errors; err != "Expected `1` to differ by one from `10`" {
+		t.Errorf("incorrect error output for custom matcher: %s", err)
+	}
+}
+
+func TestCustomMatcherPassesWithNoError(t *testing.T) {
+	out := new(output)
+	expectation := &Expectation{out, 1}
+	expectation.To("differ by one from", differsByOne, 2)
+	if err := out.errors; err != "" {
+		t.Errorf("non-blank error '%s' even though it should have passed", err)
+	}
+}
+
+var expect Expect
+
+func ExampleExpectation_To() {
+	divisibleBy := func(a, b interface{}) bool {
+		return a.(int)%b.(int) == 0
+	}
+	expect(9).To("be divisible by", divisibleBy, 3)
+}
---
+++ matchers.go
@@ -0,0 +1,58 @@
+package mao
+
+import (
+	"fmt"
+)
+
+type Expect func(val interface{}) *Expectation
+
+type formatter interface {
+	PrintTitle()
+	PrintError(string)
+}
+
+type Expectation struct {
+	Output formatter
+	Value  interface{}
+}
+
+type Matcher func(a, b interface{}) bool
+
+func Equal(a, b interface{}) bool {
+	return a == b
+}
+
+func NotEqual(a, b interface{}) bool {
+	return a != b
+}
+
+func NotExist(a, b interface{}) bool {
+	return a == nil
+}
+
+func Exist(a, b interface{}) bool {
+	return a != nil
+}
+
+func (self *Expectation) To(desc string, match Matcher, value interface{}) {
+	self.Output.PrintTitle()
+	if !match(self.Value, value) {
+		self.Output.PrintError(fmt.Sprintf("Expected `%v` to %s `%v`", self.Value, desc, value))
+	}
+}
+
+func (self *Expectation) ToEqual(b interface{}) {
+	self.To("equal", Equal, b)
+}
+
+func (self *Expectation) ToNotEqual(b interface{}) {
+	self.To("not equal", NotEqual, b)
+}
+
+func (self *Expectation) ToExist() {
+	self.To("exist", Exist, nil)
+}
+
+func (self *Expectation) ToNotExist() {
+	self.To("not exist", NotExist, nil)
+}
